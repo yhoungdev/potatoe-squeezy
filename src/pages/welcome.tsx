57be74f90886d018a8f322e826c902466cd455ed
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-24 08:55:34 +0100

    chore: added content script to append zap button ⚡⚡⚡

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -6,17 +6,17 @@ function Welcome(props) {
 		<div
 			className={`
             container flex flex-col items-center justify-center h-[100vh]
-            bg-gradient-to-b from-black to-orange-600/10
+            bg-gradient-to-b  px-[2em]from-black to-orange-600/10
         `}
 		>
 			<div className={'mb-4'}>
 				<h1 className={'text-6xl text-center animate-pulse'}>🥔</h1>
 				<p className={'mt-2 text-center text-white'}>
 					Your GitHub just got crispy. When life hands you potatoes, squeeze out
-					Bitcoin instead! 🎉🥔
+					Bitcoin instead! 🎉🥔.
 				</p>
 			</div>
-			<Button className={'w-[300px] rounded-none'}>Get Started</Button>
+			<Button className={'w-[90%] px-4 rounded-none'}>Get Started</Button>
 		</div>
 	);
 }
