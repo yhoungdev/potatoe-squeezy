96f135cd5fee61d39684a64a8a37f14e2a282d47
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-26 10:38:20 +0100

    bug: fixed icon bug

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -7,7 +7,9 @@ import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), icon()],
 });
