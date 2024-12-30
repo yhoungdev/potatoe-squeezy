9927762dda01cf535416033407d5b42b899e5007
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-30 19:14:10 +0100

    bug: trying to get auth token from registered domain

--- apps/extension/src/background.ts
+++ apps/extension/src/background.ts
@@ -1,7 +1,38 @@
 import browser from "webextension-polyfill";
 
-console.log("Hello from the background!");
+const defaultUrl = 'http://localhost:4321/';
+
+console.log("Potatoe from the background!");
 
 browser.runtime.onInstalled.addListener((details) => {
-  console.log("Extension installed:", details);
+    console.log("Extension installed:", details);
+
+    getCookiesForDomain(defaultUrl);
 });
+
+async function getCookiesForDomain(domain) {
+    try {
+        const cookieStores = await browser.cookies.getAllCookieStores();
+        const token = await  browser.cookies.getAll({domain});
+        console.log("Cookies:", token , "=====");
+        for (let store of cookieStores) {
+            if (store.domain === domain) {
+                const cookies = await browser.cookies.getAll({ domain });
+
+                if (cookies.length > 0) {
+                    console.log(`Cookies for domain ${domain}:`, cookies);
+                    cookies.forEach(cookie => {
+                        console.log(`Cookie name: ${cookie.name}, Cookie value: ${cookie.value}`);
+                    });
+                } else {
+                    console.log(`No cookies found for domain ${domain}`);
+                }
+                return;
+            }
+        }
+
+        console.log(`No matching cookie store found for domain ${domain}`);
+    } catch (error) {
+        console.error("Error fetching cookies:", error);
+    }
+}
--- apps/extension/src/pages/index.tsx
+++ apps/extension/src/pages/index.tsx
@@ -10,9 +10,19 @@ function SignIn() {
       provider: 'github'
 
     });
-
     window.open(data?.url);
   }
+
+
+  const getUser =  async () => {
+
+    const { data: { user } } = await supabaseObject.auth.getUser();
+    console.log(user);
+
+  }
+
+
+
   return (
       <div
           className={`
@@ -34,6 +44,7 @@ function SignIn() {
             <FaGithub className="w-5 h-5 mr-2" />
             Sign in with GitHub
           </button>
+
         </div>
       </div>
   );
