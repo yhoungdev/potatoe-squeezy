248bdb370d610d3a3c6f61d62f29b91a0b5e75ce
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-03-22 03:19:22 +0100

    refactor(constants): update GITHUB_CALLBACK_URL based on environment
    Modify GITHUB_CALLBACK_URL to dynamically set the callback URL based on the NODE_ENV environment variable. This ensures the correct callback URL is used in both development and production environments.

--- apps/server/src/constants/index.ts
+++ apps/server/src/constants/index.ts
@@ -2,5 +2,7 @@ export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID!;
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET!;
 export const JWT_SECRET = process.env.JWT_SECRET!;
 export const DATABASE_URL = process.env.DATABASE_URL!;
-export const GITHUB_CALLBACK_URL = process.env.GITHUB_CALLBACK_URL!;
+export const GITHUB_CALLBACK_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://potatoe-app-production.up.railway.app/auth/callback'
+  : 'http://localhost:3000/auth/callback';
 export const FRONTEND_APP_URL = process.env.FRONTEND_APP_URL!;
