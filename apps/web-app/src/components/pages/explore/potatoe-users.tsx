790edb6587fc0d5fea21502ec38c5a7f3fe91887
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-04-01 23:29:35 +0100

    updated pnpm

--- apps/web-app/src/components/pages/explore/potatoe-users.tsx
+++ apps/web-app/src/components/pages/explore/potatoe-users.tsx
@@ -1,6 +1,18 @@
+import { GithubUserSearch } from "@/components/search/GithubUserSearch";
+import { useState , } from "react";
 const PotatoeUsers = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+      const [users, setUsers] = useState<GitHubUser[]>([]);
+      const [loading, setLoading] = useState(false);
   return (
-    <div>Loading</div>
+    <div>
+        <GithubUserSearch
+                searchQuery={searchQuery}
+                onSearchChange={setSearchQuery}
+                onSearch={() => {}}
+                loading={loading}
+              />
+    </div>
   )
 }
 
