32757952610f7f1c9013c12968a06671c567d354
Alternate Universe Jean Basquiat. <54102389+yhoungdev@users.noreply.github.com>
2026-03-14 07:06:24 +0100

    bug: fixing vercel peer depissue

--- apps/web-app/dev-dist/sw.js
+++ apps/web-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a72c06ba'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5hoaekphbak"
+    "revision": "0.jeq2uqk0cmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
